bfeffa18e4052ac8fc8dca0bbede2818da50798c
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-17 20:36:01 +0100

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Car {
+
+    //Attribute/Eigenschaften
+    private String color; //Farbe
+    private String brand; //Marke
+    private int horsePower; //PS-Zahl
+
+    //Methoden
+    public void drive() {
+        System.out.println("Das Auto faehrt...");
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car(); //Instanziierung eines Objekts
+        car1.drive();
+
+    }
+}
